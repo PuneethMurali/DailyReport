7d876869f899628b17c391eaf87807ae6bfd3edb
PuneethMurali <65094114+PuneethMurali@users.noreply.github.com>
2023-12-29 12:12:04 +0530

    Add files via upload

---
+++ Day11/Day11/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    internal class Class1
+    {
+        public void Fun1() { }
+        protected void Fun2() { }
+    }
+}
