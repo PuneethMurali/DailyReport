68d20487f70b801aba0aa173881a4af83d9cdb25
PuneethMurali <65094114+PuneethMurali@users.noreply.github.com>
2024-01-03 09:09:25 +0530

    Add files via upload

---
+++ Day13/Day13/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13
+{
+    internal class Class1
+    {
+        public void Fun1() { }
+        protected void Fun2() { }
+    }
+}
---
+++ Day15/Jan2OOPEXample/Jan2OOPEXample/TestSealed.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jan2OOPEXample
+{
+    sealed class TestSealed
+    {
+        private int num1 = 100; 
+        public int Ans {  get; set; }
+        public void GetData() { }
+    }
+    public class ClientSealed{
+        static void Main(string[] args)
+        {
+            TestSealed ts = new TestSealed();
+            ts.GetData();
+           
+        }
+    }
+}
